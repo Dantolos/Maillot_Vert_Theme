188a10186db868d8fdddacecf05da7589fd26ddd
Dantolos <45291569+Dantolos@users.noreply.github.com>
2024-01-18 17:05:11 +0100

    1.0.05

--- extensions/acf/footer_page_fields.acf.php
+++ extensions/acf/footer_page_fields.acf.php
@@ -1,8 +1,5 @@
 <?php
 add_action( 'acf/include_fields', function() {
-	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
-		return;
-	}
 
 	acf_add_local_field_group( array(
 	'key' => 'group_65a94667016b4',
